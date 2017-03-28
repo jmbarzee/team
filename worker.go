f5170d854233d75b87a9da1ee299b40525eb12a5
Jacob Barzee <jbarzee@qualtrics.com>
2017-03-28 02:41:56 -0600

    cleaned up code in worker.go, no code changes

--- worker.go
+++ worker.go
@@ -14,39 +14,3 @@ func (team *Team) workerJob(newJobs chan Job, finJobs chan Job) {
 		finJobs <- job
 	}
 }
-
-/*
-func (fs *fSetter) req(url string) (int, error) {
-	//fs.DPrintf("        URL: %v", url)
-	var requestType string
-	var jsonBody []byte
-	var err error
-	if fs.setting == -1 {
-		requestType = "DELETE"
-		jsonBody = []byte("{}")
-	} else {
-		requestType = "PUT"
-		jsonBody, err = json.Marshal(struct {
-			Dial   int    `json:"dial"`
-			Author string `json:"author"`
-		}{
-			Dial:   fs.setting,
-			Author: fs.author,
-		})
-
-	}
-	req, err := http.NewRequest(requestType, url, bytes.NewReader(jsonBody))
-	if err != nil {
-		fmt.Errorf("%#v", err)
-		return 0, err
-	}
-
-	req.Header.Set("Content-Type", "application/json")
-
-	resp, err := http.DefaultClient.Do(req)
-
-	//fs.DPrintf("        resp: %v", resp)
-	defer resp.Body.Close()
-	return resp.StatusCode, err
-}
-*/
